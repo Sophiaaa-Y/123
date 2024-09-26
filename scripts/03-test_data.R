cb8f50664b6346ba5baeecd2ce07a12f59a4a7db
Sophiaaa-Y <2438862677@qq.com>
2024-09-25 21:12:31 -0400

    123 check

---
+++ scripts/03-test_data.R
@@ -0,0 +1,61 @@
+#### Preamble ####
+# Purpose: Tests on the cleaned dataset of Toronto beaches observations to check 
+# the validity. 
+# Author: Shuangyuan Yang
+# Date: 25 September 2024
+# Contact: shuangyuan.yang@mail.utoronto.ca
+# License: MIT
+# Pre-requisites: Run the files "01-download_data.R" and "02-data_cleaning.R".
+# Any other information needed? None.
+
+
+#### Workspace setup ####
+
+#install.packages("tidyverse")
+library(tidyverse)
+
+#### Test data ####
+
+# Loading the cleaned dataset
+Observations_cleaned_data <- read_csv("outputs/analysis_data/observations_cleaned_data.csv")
+
+# Checking the class of some of the columns
+Observations_cleaned_data$`Wind speed in km/h` |> class() == "numeric"
+Observations_cleaned_data$`Waterfowl population` |> class() == "numeric"
+Observations_cleaned_data$`Turbidity of water (NTUs)` |> class() == "numeric"
+Observations_cleaned_data$`Wave Action` |> class() == "character"
+
+# Checking that the minimum value in the ID column is at least 1
+Observations_cleaned_data$ID |> min() >= 1
+
+# Checking that the minimum wind speed is at least 0 (without NA)
+Observations_cleaned_data$`Wind speed in km/h` |> min(na.rm = TRUE) >= 0
+
+# Checking that the minimum waterfowl population is at least 0 (without NA)
+Observations_cleaned_data$`Waterfowl population` |> min(na.rm = TRUE) >= 0
+
+# Checking that the minimum turbidity is at least 0 (without NA)
+Observations_cleaned_data$`Turbidity of water (NTUs)` |> min(na.rm = TRUE) >= 0
+
+# Checking the range of the wind speed (without NA)
+Observations_cleaned_data$`Wind speed in km/h` |>
+  range(na.rm = TRUE)
+
+# Checking the range of the waterfowl population (without NA)
+Observations_cleaned_data$`Waterfowl population` |>
+  range(na.rm = TRUE)
+
+# Checking the range of the water turbidity (without NA)
+Observations_cleaned_data$`Turbidity of water (NTUs)` |>
+  range(na.rm = TRUE)
+
+# Checking that only four wave action types
+Observations_cleaned_data$`Wave Action` |>
+  unique() |>
+  length() == 4
+
+# Checking the only four types of wave actions are high, moderate, low, and none.
+Observations_cleaned_data$`Wave Action` |>
+  unique() |>
+  sort() == sort(c("High", "Moderate", "Low", "None"))
+
